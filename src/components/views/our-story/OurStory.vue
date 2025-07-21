1952c2825913b61584f444d4d2b343dcaf16ae98
Dylan McIver <dmciver619@live.com>
2025-07-21 23:04:57 +0100

    Add content

--- src/components/views/our-story/OurStory.vue
+++ src/components/views/our-story/OurStory.vue
@@ -7,19 +7,19 @@ import StoryItem from './StoryItem.vue';
 
     <div class="story-timeline-vertical">
         <StoryItem description="Nicole sent Dylan the first message on Facebook" side="left" title="First Message" date="May 2015">
-            <img src="/src/assets/our-story/4d228af3-332b-4847-9aff-5b0adfef3e33.jpg" />
+            <img src="/src/assets/our-story/FirstMessage.jpg" />
         </StoryItem>
-        <StoryItem description="Behind Pickie Park on a hill overlooking Pickie and the docks, Dylan asked Nicole out" side="right" title="Boyfriend/Girlfriend" date="July 2015">
-            <img src="/src/assets/our-story/4d228af3-332b-4847-9aff-5b0adfef3e33.jpg" />
+        <StoryItem description="Between Skipperstone Beach and Pickie Play Park overlooking the pier, Dylan asked Nicole to be his girlfriend" side="right" title="Boyfriend/Girlfriend" date="July 2015">
+            <img src="/src/assets/our-story/BoyfriendGirlfriend.jpg" />
         </StoryItem>
         <StoryItem side="left" title="First Kiss" date="July 2015">
-            <img src="/src/assets/our-story/4d228af3-332b-4847-9aff-5b0adfef3e33.jpg" />
+            <img src="/src/assets/our-story/FirstKissButNotReally.png" />
         </StoryItem>
         <StoryItem description="First romantic holiday to Fuerteventura" side="right" title="First Holiday Alone" date="January 2019">
-            <img src="/src/assets/our-story/4d228af3-332b-4847-9aff-5b0adfef3e33.jpg" />
+            <img src="/src/assets/our-story/Fuerteventura.png" />
         </StoryItem>
-        <StoryItem description="Dylan got down on one knee in Singapore Botanic Gardens" side="left" title="Engagement" date="July 2025">
-            <img src="/src/assets/our-story/4d228af3-332b-4847-9aff-5b0adfef3e33.jpg" />
+        <StoryItem description="Dylan got down on one knee on the Rainforest Trail in Singapore Botanic Gardens and asked Nicole to marry him" side="left" title="Engagement" date="July 2025">
+            <img src="/src/assets/our-story/Proposal.jpg" />
         </StoryItem>
     </div>
 </template>
--- src/components/views/our-story/StoryItem.vue
+++ src/components/views/our-story/StoryItem.vue
@@ -28,69 +28,77 @@
 </script>
 
 <style>
-    .story-item-container {
-        display: flex;
-        align-items: center;
-        width: calc(50% - 1px);
-        justify-content: space-between;
 
-        &.left {
-            align-self: start;
-            border-right: 2px solid white;
+    @media screen and (min-width: 1320px) {
+        .story-item-container {
+            display: flex;
+            align-items: center;
+            width: calc(50% - 1px);
+            justify-content: space-between;
 
-            .story-item-branch {
-                &::after {
-                    content: '';
-                    display: block;
-                    width: 20px;
-                    height: 20px;
-                    background-color: whitesmoke;
-                    border-radius: 50%;
-                    place-self: end;
-                    top: -10px;
-                    position: relative;
-                    right: -11px;
+            &:not(:first-of-type) {
+                margin-top: -250px;
+            }
+
+            &.left {
+                align-self: start;
+                border-right: 2px solid white;
+
+                .story-item-branch {
+                    &::after {
+                        content: '';
+                        display: block;
+                        width: 20px;
+                        height: 20px;
+                        background-color: whitesmoke;
+                        border-radius: 50%;
+                        place-self: end;
+                        top: -10px;
+                        position: relative;
+                        right: -11px;
+                    }
                 }
             }
-        }
 
-        &.right {
-            flex-direction: row-reverse;
-            align-self: end;
-            border-left: 2px solid white;
-            
-            .story-item-branch {
+            &.right {
+                flex-direction: row-reverse;
+                align-self: end;
+                border-left: 2px solid white;
                 
-                &::before {
-                    content: '';
-                    display: block;
-                    width: 20px;
-                    height: 20px;
-                    background-color: whitesmoke;
-                    border-radius: 50%;
-                    place-self: start;
-                    top: -10px;
-                    position: relative;
-                    right: 11px;
+                .story-item-branch {
+                    
+                    &::before {
+                        content: '';
+                        display: block;
+                        width: 20px;
+                        height: 20px;
+                        background-color: whitesmoke;
+                        border-radius: 50%;
+                        place-self: start;
+                        top: -10px;
+                        position: relative;
+                        right: 11px;
+                    }
+                }
+            }
+        }
+        
+        .story-item-branch {
+            height: 2px;
+            width: 100%;
+            background: white;
+        }
+
+        .story-item {
+            max-width: 500px;
+
+            .story-item-description {
+                h2 {
+                    margin: 0;
                 }
             }
         }
     }
-    
-    .story-item-branch {
-        height: 2px;
-        width: 100%;
-        background: white;
-    }
 
-    .story-item {
-        max-width: 500px;
-
-        .story-item-description {
-            h2 {
-                margin: 0;
-            }
-        }
-    }
 
 </style>
