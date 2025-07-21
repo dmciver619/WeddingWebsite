bfa6be59fe4e05b737a9fba8cd62782fd6bb11af
Dylan McIver <dmciver619@live.com>
2025-07-21 23:58:16 +0100

    Temporarily remove timeline

--- src/components/views/our-story/StoryItem.vue
+++ src/components/views/our-story/StoryItem.vue
@@ -10,7 +10,6 @@
                 <slot></slot>
             </div>
         </div>
-        <div class="story-item-branch"></div>
     </div>
 </template>
 
@@ -33,61 +32,16 @@
         .story-item-container {
             display: flex;
             align-items: center;
-            width: calc(50% - 1px);
-            justify-content: space-between;
 
             &:not(:first-of-type) {
                 margin-top: -250px;
             }
 
-            &.left {
-                align-self: start;
-                border-right: 2px solid white;
-
-                .story-item-branch {
-                    &::after {
-                        content: '';
-                        display: block;
-                        width: 20px;
-                        height: 20px;
-                        background-color: whitesmoke;
-                        border-radius: 50%;
-                        place-self: end;
-                        top: -10px;
-                        position: relative;
-                        right: -11px;
-                    }
-                }
-            }
-
             &.right {
                 flex-direction: row-reverse;
-                align-self: end;
-                border-left: 2px solid white;
-                
-                .story-item-branch {
-                    
-                    &::before {
-                        content: '';
-                        display: block;
-                        width: 20px;
-                        height: 20px;
-                        background-color: whitesmoke;
-                        border-radius: 50%;
-                        place-self: start;
-                        top: -10px;
-                        position: relative;
-                        right: 11px;
-                    }
-                }
             }
         }
         
-        .story-item-branch {
-            height: 2px;
-            width: 100%;
-            background: white;
-        }
 
         .story-item {
             max-width: 500px;
